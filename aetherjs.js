38bc2d5aa7174f88f1e6c68f5e8e2ba54657f3f8
Bishal Shrestha <167290532+BishalABPS52@users.noreply.github.com>
2025-02-13 17:23:59 +0545

    Update aetherjs.js

--- aetherjs.js
+++ aetherjs.js
@@ -7,11 +7,13 @@ function toggleMenu() {
     }
 }
 
+
 function closeMenu(event) {
     const sidebar = document.getElementById('sidebar');
     if (sidebar.style.display === 'block' && !sidebar.contains(event.target) && !document.querySelector('.menu-button').contains(event.target)) {
         sidebar.style.display = 'none';
     }
 }
+
 
 document.addEventListener('click', closeMenu);
